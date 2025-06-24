30ebdcd600feefbbc76c19a3263c19d80c1691cd
Timothée <83133135+timlapov@users.noreply.github.com>
2025-06-24 14:17:23 +0200

    Initial commit

---
+++ src/main/java/art/lapov/project2dev/Main.java
@@ -0,0 +1,12 @@
+package art.lapov;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*".repeat(100));
+        System.out.println("Welcome to Project2Dev!");
+        System.out.println("*".repeat(100));
+
+    }
+}
